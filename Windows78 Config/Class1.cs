3e5365014a47ccdbd023ba6db81efacc6300326f
Caspar Staehlin <caspar.staehlin@bluewin.ch>
2023-02-07 16:11:54 +0100

    Add project files.

---
+++ Windows78 Config/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using Windows78_Config; //add EAGetMail namespace
+using NAudio.Wave;
+
+
+namespace Windows78_Config
+{
+public class Recorder {
+
+    
+}
+}
+
